09981e2c9e02af99fb6a345eb372e8d374db5eeb
Joseph Jessie Onate <josephjessie97@gmail.com>
2019-05-14 14:51:42 +0800

    Change Detection

--- Change Detection/ChangeDetection.py
+++ Change Detection/ChangeDetection.py
@@ -1,8 +1,8 @@
 import cv2
 import numpy as np 
 
-im_file1 ="Fig0230a.tif"
-im_file2 ="Fig0230b.tif"
+im_file1 ="Input1.png"
+im_file2 ="Input2.png"
 
 img = cv2.imread(im_file1,1)
 img2 = cv2.imread(im_file2,1)
@@ -11,16 +11,16 @@ output = cv2.imread(im_file1,1)
 # get image properties.
 h1, w1, bpp1 = np.shape(img)
 
-# iterate over the entire image and mask the image.
+# iterate over the entire image and detect change in pixels.
 for py in range(0, h1):
     for px in range(0, w1):
-        cval = img[py][px][0] * (img2[py][px][0] / 255)
+        cval = img[py][px][0] - img2[py][px][0]
+        cval=abs(int(cval))
+        if cval>=215:
+            cval=0
         output[py][px][0] = cval
         output[py][px][1] = cval
         output[py][px][2] = cval
 
-# resize image
-img = cv2.resize(output, (882, 674)) 
-
-cv2.imwrite('output.jpg', output)
+cv2.imwrite('SampleOutput.jpg', output)
 cv2.waitKey(0)
